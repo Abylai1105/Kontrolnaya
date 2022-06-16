e66e11fdb584ed568649a65d0088765f2f0da8d2
Abylai <aba.omar@mail.ru>
2022-06-16 20:10:33 +0600

    Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Console.WriteLine("Введите элементы массива: ");
+string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+string[] resultArray = GetLenSelectedArray(array, 3);
+
+Console.WriteLine(string.Join(", ", resultArray));
+
+string[] GetLenSelectedArray(string[] arr, int charNumber)
+{
+    string[] result = new string[arr.Count(x => x.Length <= charNumber)];
+    int count = 0;
+    foreach (string el in arr)
+    {
+        if (el.Length <= charNumber)
+        {
+            result[count] = el;
+            count++;
+        }
+    }
+    return result;
+}
